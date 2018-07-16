9413037e58ffe57303e28d0634ecd01f23803303
mwfister <mwfister@gmail.com>
2018-07-16 17:26:49 +1000

    Migrating reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { Redux, combineReducers } from 'redux'
+
+import todos from './todos'
+import goals from './goals'
+import loading from './loading'
+
+export default combineReducers({
+  todos,
+  goals,
+  loading,
+})
