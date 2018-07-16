5a2ee101f2413ab72efdb2a71ae49ddc9e7c9d96
Celther <mwfister@gmail.com>
2018-07-16 20:25:55 +1000

    Migrated Components across
    Exporting the connected components by default.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux'
 
-class App extends Component {
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { handleInitialData } from '../actions/shared'
+
+class App extends React.Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
   render() {
+    const { loading } = this.props
+
+    if (loading) {
+      return <h3>Loading</h3>
+    }
+
     return (
       <div>
-        Hello World
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
